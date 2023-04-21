7864411fd10eac97a367809f15b663eba30d3118
Yannick Müller <yannick.mueller@fusiva.ch>
2023-04-21 09:05:17 +0200

    add initial spring project

---
+++ src/main/java/dev/ynnk/m295/M295Application.java
@@ -0,0 +1,13 @@
+package dev.ynnk.m295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M295Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M295Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ynnk/m295/M295ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ynnk.m295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
